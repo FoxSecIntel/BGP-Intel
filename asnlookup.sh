d203ce70cf588c12aa11b6b89cf419d939eea173
13gbc <118615125+13gbc@users.noreply.github.com>
2022-12-10 12:59:01 +0100

    Update asnlookup.sh

--- asnlookup.sh
+++ asnlookup.sh
@@ -2,8 +2,12 @@
 
 # Find the ASN of an IP address
 
-# Prompt the user for an IP address
-read -p "Enter an IP address: " ip_address
+if [ -z "$1" ]; then
+  echo "Error: Please provide an ip address"
+  exit 1
+fi
+
+ip_address=$1
 
 # Check if the input is a valid IP address
 if [[ $ip_address =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
